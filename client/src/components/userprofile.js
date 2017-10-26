72c9ef686c67e6d7f59286231857aed382725c7b
Trevor Wilson <tbwilson@mabankisd.net>
2017-10-26 12:06:20 -0500

    added text area requests to server

--- client/src/components/userprofile.js
+++ client/src/components/userprofile.js
@@ -5,13 +5,18 @@ export default class UserProfile extends Component {
   constructor(props){
     super(props)
     this.state = {
-      user: 'default...'
+      user: 'default...',
+      testuser: 'tbwilson',
+      contentEditText: ''
     }
     this.testclick = this.testclick.bind(this)
   }
 
   testclick(){
-    return fetch('/api/userlookup')
+    var text = this.refs.textarea.innerText
+    console.log(text)
+    let url = '/api/userlookup?' + this.state.testuser
+    return fetch('/api/userlookup?' + text)
       .then(res => res.json())
       .then((data) => {
         console.log(data)
@@ -25,8 +30,10 @@ export default class UserProfile extends Component {
     return(
       <div className='user-profile-container'>
         <div>This is a test of the user profile...</div>
+        <div className='user-lookup' contentEditable='true' ref='textarea'></div>
         <div><button onClick={this.testclick}>TestMe!</button></div>
         <div>Current User: {this.state.user}</div>
+        <div>Content Edited: {this.state.contentEditText}</div>
       </div>
     )
   }
--- index.js
+++ index.js
@@ -12,25 +12,28 @@ const url = 'mongodb://testuser:test@ds229415.mlab.com:29415/ims'
 
 app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.get('/api/userlookup', (req, res) => {
-  console.log('this api has been accessed!')
+app.get(`/api/userlookup?\*`, (req, res) => {
+  let test = req.url.slice(16).toString()
+  console.log('this api has been accessed! ' + test)
   function callback(info) {
-    console.log('Callback has been run: ' +info.user)
-    let data = { "user":info.user }
-    console.log('Callback has been run: ' +data.user)
-    res.json(info)
+    if(info = 'notfound'){
+      res.json('no user')
+    } else {
+      console.log('Callback has been run: ' +info.user)
+      res.json(info)
+    }
   }
   mongoClient.connect(url, function(err, db){
     if(err){console.log(err, 'this didn\'t work')}
     else {
-      db.collection('users').find({'user': 'tbwilson'}).toArray(function(err, res){
+      db.collection('users').find({'user': test}).toArray(function(err, res){
         if(err){console.log(err)}
         else if(res.length) {
           console.log('Found it!: ', res[0])
           db.close(callback(res[0]))
         } else {
           console.log('not found!!', res)
-          db.close()
+          db.close(callback('notfound'))
         }
       })
     }
